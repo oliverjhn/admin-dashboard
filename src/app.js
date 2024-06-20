be432e38a045bda334a903e6de49c896a8ead00b
Oliver Hogan <oliver@oliver-hogan.com>
2024-06-21 07:08:49 +1000

    such a stupid fix but it works

--- src/app.js
+++ src/app.js
@@ -5,3 +5,8 @@ menuToggle.addEventListener("click", () => {
   sidebar.classList.toggle("sidebar-closed");
   sidebar.classList.toggle("sidebar-open");
 });
+
+if (window.innerWidth > 1024) {
+  sidebar.classList.remove("sidebar-closed");
+  sidebar.classList.add("sidebar-open");
+}

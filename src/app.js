22033674490efe520cae0ea925c6d7bdd061f986
Oliver Hogan <oliver@oliver-hogan.com>
2024-07-01 16:37:37 +1000

    working

--- src/app.js
+++ src/app.js
@@ -10,3 +10,17 @@ if (window.innerWidth > 1024) {
   sidebar.classList.remove("sidebar-closed");
   sidebar.classList.add("sidebar-open");
 }
+
+// Dropdown functionality
+const sections = ["announcements", "trending", "projects"];
+
+sections.forEach((section) => {
+  const header = document.getElementById(`${section}-header`);
+  const content = document.getElementById(`${section}-content`);
+  const chevron = header.querySelector("img");
+
+  header.addEventListener("click", () => {
+    content.classList.toggle("open");
+    chevron.classList.toggle("rotate-180");
+  });
+});
